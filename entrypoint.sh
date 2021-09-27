e1f747e5835d292b35a82614308cbe5d0bc45eb5
SBub <svetaibuben@gmail.com>
2021-09-27 17:45:28 +0200

    ref(entrypoint): udpating sh script to create pr with a v update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,21 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_FOLDER" ]
-then
-  echo "Source folder must be defined"
-  return -1
-fi
-
-if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
-then
-  echo "Destination head branch cannot be 'main' nor 'master'"
-  return -1
-fi
-
-if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
-then
-  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
-else
-  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
-fi
+get_value_from_package()
+{
+  PROP=$1
+  cat package.json \
+  | grep $PROP \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]'
+}
 
 CLONE_DIR=$(mktemp -d)
+PACKAGE_VERSION="$(get_value_from_package version)"
+PACAKGE_NAME="$(get_value_from_package name)"
+DESTINATION_BRANCH="chore/bump-$PACAKGE_NAME-$PACKAGE_VERSION-$(date +%s)"
 
 echo "Setting git variables"
 export GITHUB_TOKEN=$API_TOKEN_GITHUB
@@ -33,24 +28,15 @@ echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
-cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
+mkdir -p $CLONE_DIR
 cd "$CLONE_DIR"
-git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
+git checkout -b $DESTINATION_BRANCH
 
 echo "Adding git commit"
-git add .
-if git status | grep -q "Changes to be committed"
-then
-  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-  echo "Pushing git commit"
-  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-  echo "Creating a pull request"
-  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b $INPUT_DESTINATION_HEAD_BRANCH \
-               -B $INPUT_DESTINATION_BASE_BRANCH \
-               -H $INPUT_DESTINATION_HEAD_BRANCH \
-                  $PULL_REQUEST_REVIEWERS
-else
-  echo "No changes detected"
-fi
+git commit -m "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA" --allow-empty
+git push -u origin HEAD:$DESTINATION_BRANCH
+echo "Creating a pull request"
+gh pr create -t "$PACAKGE_NAME was updated to v$PACKAGE_VERSION" \
+              -b "You should bump the version of $PACAKGE_NAME to $PACKAGE_VERSION to use latest updates" \
+              -B $INPUT_DESTINATION_BASE_BRANCH \
+              -H $DESTINATION_BRANCH \
